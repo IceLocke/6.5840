b6d26955a73d3391c369a1d28238b58cda14c5cc
IceLocke <lockeice@gmail.com>
2025-07-23 22:52:32 +0800

    Finish `AssignTask` and `FinishTask` RPC definitions

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -24,6 +26,39 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+//
+// RPC definitions for the coordinator to communicate with workers.
+// All RPCs are from workers to coordinator.
+//
+
+// Worker -> Coordinator, ask for a task
+type AssignTaskArgs struct {
+	WorkerID int
+}
+
+// Coordinator -> Worker, reply with a task.
+// MapTaskID is given with a KV pair if the worker is assigned a map task,
+// ReduceTaskID and MapTaskID are both given if the worker is assigned a reduce task.
+type AssignTaskReply struct {
+	TaskType      int // 0 for map task, 1 for reduce task
+	NReduce       int // number of reduce tasks
+	MapTaskID     int
+	ReduceTaskID  int
+	Intermediates []int // prefixes of intermediate files for reduce task, i.e. map task IDs
+	Filename      string   // filename for map task
+}
+
+// Worker -> Coordinator, report that a task is complete
+type CompleteTaskArgs struct {
+	TaskType      int // 0 for map task, 1 for reduce task
+	MapTaskID     int
+	ReduceTaskID  int
+	Intermediates []int // suffixes of intermediate files for reduce task, i.e. reduce task IDs
+}
+
+// Coordinator -> Worker, reply to task completion
+type CompleteTaskReply struct {
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
